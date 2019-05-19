4aa32e6224ffe0a55a648aacddf94c1f9e6c8b6e
Michelle Pham <m_vpham@M-vpham.local>
2019-05-18 22:31:39 -0400

    added in initial views

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+/**You import the containers into this file */
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './components/FirstComponent';
+import OpeningPage from './container/OpeningPage';
+import filterPage from './container/filterPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <OpeningPage></OpeningPage>
+          <FirstComponent displaytext="First Component Data"/>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
