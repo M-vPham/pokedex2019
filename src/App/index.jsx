09cca6c99c9a2f11be2ac0b9a4569408ced1f453
ktsuench <will289@hotmail.com>
2019-05-26 03:25:15 -0400

    updated App landing page display and layout

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
 // eslint-disable-next-line import/imports-first
-import { Button, Columns, Column } from 'bloomer';
+import { Button, Columns, Column, Container, Section } from 'bloomer';
 import './index.css';
 import AppHeader from './components/AppHeader.jsx';
 import DirectoryPage from './components/DirectoryPage.jsx';
@@ -17,18 +17,44 @@ const App = () => {
       <AppHeader />
       <main>
         <Router history={history}>
-          <Columns isCentered isVCentered>
-            <Column isSize={{ mobile: 1 / 2, default: 12 }}>
-              <Button href="/directory" isColor="danger" isSize="large" isOutlined>
-                Directory
-              </Button>
-              <Button href="/teambuilder" isColor="danger" isSize="large" isOutlined>
-                Team Builder
-              </Button>
-            </Column>
-            <Route path="/directory" component={DirectoryPage} />
-            <Route path="/teambuilder" component={TeamBuilderPage} />
-          </Columns>
+          <Section>
+            <Container>
+              <Columns isCentered isVCentered>
+                <Column
+                  className="App-Nav-Container"
+                  isSize={{ mobile: '3/4', tablet: 4, desktop: 3, widescreen: 3, default: 6 }}
+                >
+                  <Button
+                    className="App-Nav-Button"
+                    href="/directory"
+                    isColor="danger"
+                    isSize="large"
+                    isOutlined
+                  >
+                    Pokedex
+                  </Button>
+                </Column>
+              </Columns>
+              <Columns isCentered isVCentered>
+                <Column
+                  className="App-Nav-Container"
+                  isSize={{ mobile: '3/4', tablet: 4, desktop: 3, widescreen: 3, default: 6 }}
+                >
+                  <Button
+                    className="App-Nav-Button"
+                    href="/teambuilder"
+                    isColor="danger"
+                    isSize="large"
+                    isOutlined
+                  >
+                    Team Builder
+                  </Button>
+                </Column>
+                <Route path="/directory" component={DirectoryPage} />
+                <Route path="/teambuilder" component={TeamBuilderPage} />
+              </Columns>
+            </Container>
+          </Section>
         </Router>
       </main>
     </div>
