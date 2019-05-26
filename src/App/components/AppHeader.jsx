69ba65e004f6e451f03e36296160ac7bbbf45426
ktsuench <will289@hotmail.com>
2019-05-26 00:09:05 -0400

    fixed styling of the app header

--- src/App/components/AppHeader.jsx
+++ src/App/components/AppHeader.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 // import {Router, Route, Link} from 'react-router-dom';
-import { Navbar, NavbarBrand, NavbarItem } from 'bloomer';
+import { Navbar, NavbarBrand, NavbarItem, NavbarMenu, Title } from 'bloomer';
 
 const AppHeader = () => {
   return (
-    <Navbar>
-      <NavbarBrand style={{ 'background-color': 'rgb(244, 66, 66)' }}>
-        <NavbarItem style={{ 'margin-left': 'auto', 'margin-right': 'auto', color: 'white' }}>
-          PokeGear
+    <Navbar style={{ background: 'rgb(244, 66, 66)' }}>
+      <NavbarBrand>
+        <NavbarItem isUnselectable>
+          <Title isSize={3} tag="h1" hasTextColor="white">
+            PokeGear
+          </Title>
         </NavbarItem>
       </NavbarBrand>
+      <NavbarMenu />
     </Navbar>
   );
 };
